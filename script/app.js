fc01ea693b0faee8ed89d78bedb74f686afabda5
jvanhook93 <106854412+jvanhook93@users.noreply.github.com>
2022-07-11 20:43:36 -0600

    Setting up project

---
+++ script/app.js
@@ -0,0 +1,21 @@
+//create the User constructor
+class User {
+    constructor(fname, lname, email, password) {
+        this.firstname = fname;
+        this.lastname = lname;
+        this.email = email;
+        this.password = password;
+    }
+
+}
+//create the register function
+function register(){
+    let userName=$("#txtFirstName").val();
+    let userLname=$("#txtLastName").val();
+    let userEmail=$("txtEmail").val();
+    let userPass=$("#txtPassword").val();
+}
+    //get the values from the inputs
+    //create the obj
+    //display the user on the console
+    console.log()
